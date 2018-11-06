c938e3994521287d783556f6917b0e4c624e20c8
bravo <benzebgong@gmail.com>
2018-11-05 20:11:38 -0800

    update curve requirement

--- openssl.sh
+++ openssl.sh
@@ -1,2 +1,2 @@
-openssl ecparam -genkey -name brainpoolP512t1 -out ./mycert/server.key
+openssl ecparam -genkey -name secp384r1 -out ./mycert/server.key
 openssl req -new -x509 -sha256 -key ./mycert/server.key -out ./mycert/server.crt -days 3650
